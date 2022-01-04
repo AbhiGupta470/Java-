b0dcc13415327915c6d03bc8adc9213ff2795c45
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-01-05 00:57:17 +0530

    Create Week2_Second_CheckTriangle.java

---
+++ Week2_Second_CheckTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int side1 = sc.nextInt();
+    int side2 = sc.nextInt();
+    int side3 = sc.nextInt();
+    if(side1==side2 && side2==side3 && side3==side1)
+    System.out.println("Equilateral");
+    else if(side1!=side2 && side2!=side3 && side3!=side1)
+    System.out.println("Scalene");
+    else
+    System.out.println("Isoceles");
+    
+	}
+    
+}
