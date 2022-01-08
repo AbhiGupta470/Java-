d682559c7ba28584e68c7676fbf0d345f481e194
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-01-08 23:33:58 +0530

    Create Harry_TraverseArrayForEachLoop.java

---
+++ Harry_TraverseArrayForEachLoop.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int[] marks = {34, 45, 56, 67, 76};
+        //Traversing ana array using for each loop
+        for(int element:marks)
+        {
+            System.out.println(element);
+        }
+    }
+}
