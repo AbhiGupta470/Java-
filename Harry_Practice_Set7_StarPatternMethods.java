3b55c431f8cf25a86b360ea8c2ace10a2233621d
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-01-10 19:27:54 +0530

    Create Harry_Practice_Set7_StarPatternMethods.java

---
+++ Harry_Practice_Set7_StarPatternMethods.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+    static void Pattern(int n)
+    {
+          for(int i=1;i<=n;i++)
+          {
+              for(int j=1;j<=i;j++)
+                  System.out.print("* ");
+              System.out.println("");
+          }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        Pattern(num);
+    }
+}
