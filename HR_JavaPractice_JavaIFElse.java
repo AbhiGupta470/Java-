59c78462a19321c5373fef4ad16e3a5837fe392f
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-01-15 00:34:54 +0530

    Create HR_JavaPractice_JavaIFElse.java

---
+++ HR_JavaPractice_JavaIFElse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Solution
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num%2!=0)
+            System.out.println("Weird");
+        else if(num%2==0 && num>=2 && num<=5)
+            System.out.println("Not Weird");
+        else if(num%2==0 && num>=6 && num<=20)
+            System.out.println("Weird");
+        else if(num%2==0 && num>20)
+            System.out.println("Not Weird");
+    }
+}
