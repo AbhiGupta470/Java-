291472f20f6d701dfc12e238dbe788221bbc7951
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-01-18 02:05:44 +0530

    Create Neeraj_DSA_DeletingAnElementAtTheSpecifiedPosition.java

---
+++ Neeraj_DSA_DeletingAnElementAtTheSpecifiedPosition.java
@@ -0,0 +1,24 @@
+//Write a java program to delete an element from array at a specified position which will be given by the user
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the position from which you want to delete the element from the array: ");
+        int index = sc.nextInt();
+        int store = arr[index];
+        for(int i= position-1+1;i<arr.length;i++)
+        {
+            arr[i-1] = arr[i];
+        }
+        System.out.println("After deleting the element at the specified position the array becomes: ");
+        for (int i = 0; i < arr.length-1; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
