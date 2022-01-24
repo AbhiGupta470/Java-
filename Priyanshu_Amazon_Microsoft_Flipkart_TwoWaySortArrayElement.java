44f9392de279c9c8d38a7f7fa273b7eb136dd10f
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-01-25 01:10:14 +0530

    Create Priyanshu_Amazon_Microsoft_Flipkart_TwoWaySortArrayElement.java

---
+++ Priyanshu_Amazon_Microsoft_Flipkart_TwoWaySortArrayElement.java
@@ -0,0 +1,46 @@
+/*
+Given a sorted array arr[] of distinct integers. Sort the array into a wave-like array and return it
+In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5].....
+
+If there are multiple solutions, find the lexicographically smallest one.
+
+Example 1:
+
+Input:
+n = 5
+arr[] = {1,2,3,4,5}
+Output: 2 1 4 3 5
+Explanation: Array elements after 
+sorting it in wave form are 
+2 1 4 3 5.
+Example 2:
+
+Input:
+n = 6
+arr[] = {2,4,7,8,9,10}
+Output: 4 2 8 7 10 9
+Explanation: Array elements after 
+sorting it in wave form are 
+4 2 8 7 10 9.
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int temp = 0;
+        for(int i=0;i<arr.length-1;i+=2)
+        {
+            temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
