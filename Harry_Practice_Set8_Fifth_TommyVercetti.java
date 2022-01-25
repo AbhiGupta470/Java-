df2cf08cf0badd4180111c6c66ba00a62c14e508
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-01-26 01:34:52 +0530

    Create Harry_Practice_Set8_Fifth_TommyVercetti.java

---
+++ Harry_Practice_Set8_Fifth_TommyVercetti.java
@@ -0,0 +1,22 @@
+public class Tommyvercetti
+{
+    public void Hitiing()
+    {
+        System.out.println("Hitting");
+    }
+    public void Running()
+    {
+        System.out.println("Running");
+    }
+    public void Firing()
+    {
+        System.out.println("Firing");
+    }
+
+    public static void main(String[] args) {
+        Tommyvercetti obj = new Tommyvercetti();
+        obj.Hitiing();
+        obj.Firing();
+        obj.Running();
+    }
+}
